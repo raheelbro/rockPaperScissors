421c83066c141cd0f3ae2031fa564846cb7159cd
Raheel <raheelbro99@gmail.com>
2022-10-22 10:57:27 -0400

    define playRound function

--- js.js
+++ js.js
@@ -3,9 +3,52 @@
 
 
 function getComputerChoice(){
-  let textArray = ["Rock", "Paper", "Scissors"];
+  let textArray = ["rock", "paper", "scissors"];
   let randomElement = textArray[Math.floor(Math.random() * textArray.length)];
   return randomElement;
 }
 
-console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection) {
+  let p = playerSelection.toLowerCase();
+  let c = computerSelection.toLowerCase();
+
+  if (p === c){
+    console.log(`You: ${p}\nComputer: ${c}`);
+    
+    console.log("Tied\n");
+  }
+  else if(p === "rock" && c === "paper"){
+    console.log(`You: ${p}\nComputer: ${c}`);
+    console.log("You Lose! Paper Beats Rock");
+  }
+  else if(p === "rock" && c === "scissors"){
+    console.log(`You: ${p}\nComputer: ${c}`);
+    console.log("You Win! Rock Beats Scissors");
+  }
+  else if(p === "paper" && c === "rock"){
+    console.log(`You: ${p}\nComputer: ${c}`);
+    console.log("You Win! Paper Beats Rock");
+  }
+  else if(p === "paper" && c === "scissors"){
+    console.log(`You: ${p}\nComputer: ${c}`);
+    console.log("You Lose! Scissors Beat Paper");
+  }
+  else if(p === "scissors" && c === "rock"){
+    console.log(`You: ${p}\nComputer: ${c}`);
+    console.log("You Lose! Rock Beats Scissors");
+  }
+  else if(p === "scissors" && c === "paper"){
+    console.log(`You: ${p}\nComputer: ${c}`);
+    console.log("You Win! Scissors Beat Paper");
+  }
+  else{
+    console.log("Invalid Entry");
+  }
+}
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+playRound(playerSelection, computerSelection);
