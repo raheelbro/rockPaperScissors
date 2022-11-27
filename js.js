33d99845770eac0fd906a25ef4a607385fe30ddf
Raheel <raheelbro99@gmail.com>
2022-11-27 00:58:05 -0500

    implement 5 round functionality

--- js.js
+++ js.js
@@ -9,7 +9,7 @@ function getComputerChoice() {
 const listItems = document.querySelectorAll('.list-item img');
 const li = document.querySelector('.list-item');
 //console.log(li.classList.add('item-clicked'));
-
+let turns = 0;
 
   listItems.forEach(item => {
     item.addEventListener('click',()=> {
@@ -21,39 +21,77 @@ const li = document.querySelector('.list-item');
       
       if(item.alt == 'rock' || item.alt == 'paper' || item.alt == 'scissors'){
         playerSelection = item.alt.toLowerCase();
-        console.log(playerSelection);
+        // console.log("player: ", playerSelection);
+        // console.log("computer", getComputerChoice());
       }
-      // else if(item.alt == 'paper'){
 
-      // }
-     //  console.log(item.alt);
       
-    });
+      playRound(playerSelection, getComputerChoice());
+      
 
+      
+      
+    });   
+  })
 
-    
-
-        
-    
-  });
-
-
-
-// let array = [...listItems];
-// //console.log(array[1].alt);
-
-// array.forEach(item => {
-//   console.log(typeof(item.alt));
-
-// });
 
 
 
 
 function playRound(playerSelection, computerSelection) {
-  let p = playerSelection.toLowerCase();
-  let c = computerSelection.toLowerCase();
+  let playerScore = parseInt(document.querySelector('.p-score-count').textContent);
+  let computerScore = parseInt(document.querySelector('.c-score-count').textContent);
+  //console.log(playerScore);
 
+  
+  let p = playerSelection;
+  let c = computerSelection;
+
+  
+
+  if((p == 'rock' && c == 'paper') ||
+    (p == 'paper' && c == 'scissors') ||
+    (p =='scissors' && c =='rock')) {
+      console.log('player: ', p);
+      console.log('computer: ', c);
+
+    computerScore = computerScore + 1;
+    document.querySelector('.c-score-count').textContent = computerScore;
+    turns = turns + 1;
+}
+else if((p == 'rock' && c == 'scissors') ||
+    (p == 'paper' && c == 'rock') ||
+    (p =='scissors' && c =='paper')) { 
+      console.log('player: ', p);
+      console.log('computer: ', c);
+      playerScore = playerScore + 1;
+      document.querySelector('.p-score-count').textContent = playerScore;
+      turns = turns + 1;
+  }
+  else {
+    console.log('player: ', p);
+      console.log('computer: ', c);
+      turns = turns + 1;
+  }
+  
+
+
+  if(turns == 5){
+    if(playerScore > computerScore){
+      console.log('You Win!');
+    }
+    else if(playerScore < computerScore){
+      console.log('You Lose!');
+  }
+  else{
+    console.log('Tie');
+  }
+}
+else{
+  console.log(turns);
 }
 
 
+
+  
+}
